01aea5d5ad83d91e74843aa85a325a2b9091f237
ferwizawsky <ferryspedia14@gmail.com>
2021-12-05 13:17:41 +0700

    Base Project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+import './assets/scss/sb-admin-2.scss'; 
+import './assets/vendor/fontawesome-free/css/all.min.css';  
+import 'jquery/dist/jquery.min'
+import 'popper.js/dist/popper.min'
+import 'bootstrap/dist/js/bootstrap.min'
+
+ 
+
 Vue.config.productionTip = false
 
 new Vue({
